91d746e0ced9869ec4ccb1960dc9e11b5f8c2bd6
LaupWing <loc_strict@hotmail.com>
2020-03-25 13:19:41 +0100

    first commit

---
+++ node/basic_sessions/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const session = require('express-session')
+app
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({extended: true}))
+    .use(express.static('public'))
+    .use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { 
+            secure: false,
+            sameSite: true,
+            maxAge: 3600000
+        }
+    }))
+    .post('/login', (req,res)=>{
+        req.session.email = req.body.email
+        req.session.password = req.body.password
+        console.log(req.session.email, req.session.password)
+        console.log(req.session)
+        res.redirect('/home')
+    })
+    .get('/home', (req,res)=>{
+        console.log(req.session.email)
+        res.send('hello')
+    })
+    .listen(3030, ()=>console.log('Server is listening'))
